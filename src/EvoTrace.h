1a5bb90df0b718abac312b5f9dac42770587d6c4
Kar98Keanu <1914tkk@gmail.com>
2025-04-15 12:37:50 +0800

    Updated library properties

--- src/EvoTrace.h
+++ src/EvoTrace.h
@@ -2,7 +2,7 @@
 #define EVO_TRACE_H
 
 #include <Evo.h>
-#include "sensors/EvoTCS34725.h"  // Corrected path to include EvoTCS34725
+#include "sensors/EvoTCS34725.h"
 #include "motors/EvoMotor.h"
 #include "robotics/EvoMotorPair.h"
 
